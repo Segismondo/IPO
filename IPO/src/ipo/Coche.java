3ec2f3ca52dec81d7bb9832012cd07879682b1e2
Adrian Diez <adri10alamo@gmail.com>
2018-07-05 15:44:00 +0200

    First Commit

---
+++ IPO/src/ipo/Coche.java
@@ -0,0 +1,78 @@
+package ipo;
+
+import java.io.*;
+import static java.lang.Integer.parseInt;
+
+public class Coche {
+
+    private String marca;
+    int anio;
+    private String modelo;
+    private float precio;
+
+    public Coche (String m, int a, String _modelo, float p){ 
+        marca=m;
+        anio=a;
+        modelo=_modelo;
+        precio=p;
+    }
+
+    
+    
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    /**
+     * @return the anio
+     */
+    public int getAnio() {
+        return anio;
+    }
+
+    /**
+     * @param anio the anio to set
+     */
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the precio
+     */
+    public float getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+}
